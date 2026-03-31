7a1dcc212d873f251b9f4097a84ef11ecc44b79a
AthillaDev <athillacruzp@gmail.com>
2026-03-31 14:23:16 -0300

    projeto-matio-luigi

---
+++ scripts.js
@@ -0,0 +1,16 @@
+
+
+const form = document.querySelector(".formulario-fale-conosco")
+const mascara = document.querySelector(".mascara-formulario")
+
+function cliqueiNoBotão() {
+    form.style.left = "50%"
+    form.style.transform = "translateX(-50%)"
+    mascara.style.visibility = "visible"
+}
+
+function esconderform() {
+    mascara.style.visibility = "hidden"
+    form.style.left = "-300px"
+    form.style.transform = "translateX(0)"
+}
