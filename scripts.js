d55b9058a07b18591742b3293b179339923be607
AthillaDev <athillacruzp@gmail.com>
2026-04-01 12:40:45 -0300

    alteraçao na descriçao do arquivo assets

--- scripts.js
+++ scripts.js
@@ -3,12 +3,14 @@
 const form = document.querySelector(".formulario-fale-conosco")
 const mascara = document.querySelector(".mascara-formulario")
 
+// Abrir formulário
 function cliqueiNoBotão() {
     form.style.left = "50%"
     form.style.transform = "translateX(-50%)"
     mascara.style.visibility = "visible"
 }
 
+// Fechar formulário
 function esconderform() {
     mascara.style.visibility = "hidden"
     form.style.left = "-300px"
